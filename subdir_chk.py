f7da507f129c9a318a1d1728fb297e2c492e7789
JRMhere <joshua6781@gmail.com>
2020-08-31 20:58:59 -0500

    31 Aug 2020

--- subdir_chk.py
+++ subdir_chk.py
@@ -1,6 +1,7 @@
 #Checks if the set of subdirectories in two different directories is identical.
 #Returns two lists - one of subdirs that are in directory A but not directory B, and one of subdirs in directory B but not A.
 import os
+import re
 
 
 def get_subdirs(dir1, dir2):
@@ -13,5 +14,14 @@ def find_diffs(x1, x2):
     y2 = list(set(x2)-set(x1)) #Things in x2 that aren't in x1
     return [y1, y2]
 
-
-
+def extractInts(x):
+    #Extracts the integers from all strings in the passed list.
+    i = 0
+    for s in x:
+        try:
+            s = int(re.search(r'\d+', s).group())
+            x[i] = s
+            i += 1
+        except:
+            print("Exception")
+    return x
